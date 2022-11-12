45f6a29a9269ffc00ed28fa54d9d8be83e0b2e85
andriitomilov <andriitomilov@ukr.net>
2022-11-12 11:45:47 +0200

    added toggle class active to filter

--- js/filter.js
+++ js/filter.js
@@ -2,27 +2,37 @@ $(document).ready(function () {
   // filter
 
   // all
-  $(".filter__all").click(function () {
-    $(".grid__item").show();
+  $('.filter__all').click(function () {
+    $('.grid__item').show();
+    $('.filter__btn').removeClass('active');
+    $('.filter__all').addClass('active');
   });
   // web
-  $(".filter__web").click(function () {
-    $(".grid__item").hide();
-    $(".product__web").show();
+  $('.filter__web').click(function () {
+    $('.grid__item').hide();
+    $('.product__web').show();
+    $('.filter__btn').removeClass('active');
+    $('.filter__web').addClass('active');
   });
   // app
-  $(".filter__app").click(function () {
-    $(".grid__item").hide();
-    $(".product__app").show();
+  $('.filter__app').click(function () {
+    $('.grid__item').hide();
+    $('.product__app').show();
+    $('.filter__btn').removeClass('active');
+    $('.filter__app').addClass('active');
   });
   // desing
-  $(".filter__desing").click(function () {
-    $(".grid__item").hide();
-    $(".product__desing").show();
+  $('.filter__desing').click(function () {
+    $('.grid__item').hide();
+    $('.product__desing').show();
+    $('.filter__btn').removeClass('active');
+    $('.filter__desing').addClass('active');
   });
   // marketing
   $('.filter__marketing').click(function () {
     $('.grid__item').hide();
     $('.product__marketing').show();
+    $('.filter__btn').removeClass('active');
+    $('.filter__marketing').addClass('active');
   });
 });
